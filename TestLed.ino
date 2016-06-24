a29492ecf65b4e8f895f7313ced6f0bde7895f00
vblebedev <basswan@yandex.ru>
2016-06-24 15:03:28 +0300

    Clear code
    Clear code from unused in test elements

--- TestLed.ino
+++ TestLed.ino
@@ -6,64 +6,25 @@
 
 /*
  Create LetControl object, define pin connections
- We have 2 MAX72XX for eyes.
  */
 #define PIN_EYES_DIN 12
 #define PIN_EYES_CS 11
 #define PIN_EYES_CLK 10
-LedControl lc = LedControl(PIN_EYES_DIN, PIN_EYES_CLK, PIN_EYES_CS, 2);
 
-// rotation
-bool rotateMatrix0 = false;  // rotate 0 matrix by 180 deg
-bool rotateMatrix1 = false;  // rotate 1 matrix by 180 deg
-
-// define eye ball without pupil  
-byte eyeBall[8]={
-  B00111100,
-  B01111110,
-  B11111111,
-  B11111111,
-  B11111111,
-  B11111111,
-  B01111110,
-  B00111100
-};
-
-byte eyePupil = B11100111;
-
-// stores current state of LEDs
-byte eyeCurrent[8];
-int currentX;
-int currentY;
-int cntLoop = 0;
-int cntEffect = 0;
-
-// min and max positions
-#define MIN -2
-#define MAX  2
-
-// delays
-#define DELAY_BLINK 40
-
-// perform an effect every # of loop iterations, 0 to disable
-#define EFFECT_ITERATION 4
+// Testing only one Led
+LedControl lc = LedControl(PIN_EYES_DIN, PIN_EYES_CLK, PIN_EYES_CS, 1);
 
 /*
   Arduino setup
 */
 void setup() 
 {
-  // MAX72XX is in power-saving mode on startup, we have to do a wakeup call
+  // MAX7219 is in power-saving mode on startup, we have to do a wakeup call
   lc.shutdown(0,false);
-  lc.shutdown(1,false);
-
   // set the brightness to low
   lc.setIntensity(0,1);
-  lc.setIntensity(1,1);
-
-  // clear both modules
+  // clear module
   lc.clearDisplay(0);
-  lc.clearDisplay(1);
 }
 
 /*
@@ -78,8 +39,7 @@ void loop()
   {
     for (int r=0; r<=7; r++)
     {
-      setRow(0, r, b);
-      setRow(1, r, b);
+      lc.setRow(0, r, b);
     }
     b = b >> 1;
     delay(50);
@@ -88,54 +48,7 @@ void loop()
   b = B11111111;
   for (int r=0; r<=7; r++)
   {
-    setRow(0, r, b);
-    setRow(1, r, b);
+    lc.setRow(0, r, b);
   }
   delay(500);
-}
-
-/*
-  This method sets values to matrix row
-  Performs 180 rotation if needed
-*/
-void setRow(int addr, int row, byte rowValue)
-{
-  if (((addr == 0) && (rotateMatrix0)) || (addr == 1 && rotateMatrix1))
-  {
-    row = abs(row - 7);
-    rowValue = bitswap(rowValue);
-  }
-
-  lc.setRow(addr, row, rowValue);
-}
-
-
-/*
-  Reverse bits in byte
-  http://www.nrtm.org/index.php/2013/07/25/reverse-bits-in-a-byte/
-*/
-byte bitswap (byte x)
-{
-  byte result;
- 
-    asm("mov __tmp_reg__, %[in] \n\t"
-      "lsl __tmp_reg__  \n\t"   /* shift out high bit to carry */
-      "ror %[out] \n\t"  /* rotate carry __tmp_reg__to low bit (eventually) */
-      "lsl __tmp_reg__  \n\t"   /* 2 */
-      "ror %[out] \n\t"
-      "lsl __tmp_reg__  \n\t"   /* 3 */
-      "ror %[out] \n\t"
-      "lsl __tmp_reg__  \n\t"   /* 4 */
-      "ror %[out] \n\t"
- 
-      "lsl __tmp_reg__  \n\t"   /* 5 */
-      "ror %[out] \n\t"
-      "lsl __tmp_reg__  \n\t"   /* 6 */
-      "ror %[out] \n\t"
-      "lsl __tmp_reg__  \n\t"   /* 7 */
-      "ror %[out] \n\t"
-      "lsl __tmp_reg__  \n\t"   /* 8 */
-      "ror %[out] \n\t"
-      : [out] "=r" (result) : [in] "r" (x));
-      return(result);
 }
